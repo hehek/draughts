1f79a0a181be4a1285ad56fd9aa3be265ea563dc
Vadim Velikodniy <vadim@velikodniy.name>
2017-04-17 12:06:00 +0300

    Тест для GetMovesManWithoutCapture

---
+++ TestDraughts/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestDraughts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestDraughts")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9853d78-6fb8-4e85-a704-9a1e70626877")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestDraughts/UnitTestBoard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Draughts;
+
+namespace TestDraughts
+{
+    [TestClass]
+    public class UnitTestBoard
+    {
+        [TestMethod]
+        public void TestGetMovesManWithoutCapture()
+        {
+            List<Move> moves;
+            List<Coord> new_coords, exp_coords;
+            Board board;
+            PrivateObject pboard;
+
+
+            board = Board.Init("d4", "", "", "");
+            pboard = new PrivateObject(board);
+            moves = pboard.Invoke("GetMovesManWithoutCapture", new Coord("d4")) as List<Move>;
+            exp_coords = new List<Coord>() { new Coord("c5"), new Coord("e5") };
+            new_coords = (from m in moves select m.new_pos).ToList();
+            CollectionAssert.AreEquivalent(exp_coords, new_coords);
+
+            board = Board.Init("", "d4", "", "");
+            pboard = new PrivateObject(board);
+            moves = pboard.Invoke("GetMovesManWithoutCapture", new Coord("d4")) as List<Move>;
+            exp_coords = new List<Coord>() { new Coord("c3"), new Coord("e3") };
+            new_coords = (from m in moves select m.new_pos).ToList();
+            CollectionAssert.AreEquivalent(exp_coords, new_coords);
+
+            board = Board.Init("", "d4 c3", "", "");
+            pboard = new PrivateObject(board);
+            moves = pboard.Invoke("GetMovesManWithoutCapture", new Coord("d4")) as List<Move>;
+            exp_coords = new List<Coord>() { new Coord("e3") };
+            new_coords = (from m in moves select m.new_pos).ToList();
+            CollectionAssert.AreEquivalent(exp_coords, new_coords);
+
+            board = Board.Init("d4", "c5", "e5", "");
+            pboard = new PrivateObject(board);
+            moves = pboard.Invoke("GetMovesManWithoutCapture", new Coord("d4")) as List<Move>;
+            Assert.AreEqual(0, moves.Count);
+
+            board = Board.Init("a1", "", "", "");
+            pboard = new PrivateObject(board);
+            moves = pboard.Invoke("GetMovesManWithoutCapture", new Coord("a1")) as List<Move>;
+            exp_coords = new List<Coord>() { new Coord("b2") };
+            new_coords = (from m in moves select m.new_pos).ToList();
+            CollectionAssert.AreEquivalent(exp_coords, new_coords);
+
+            Assert.AreEqual(BoardField.WHITE, moves[0].new_board[1, 1]);
+        }
+    }
+}
